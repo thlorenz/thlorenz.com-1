18d6b535a2c84e882eba7f5b7effffe0436d5ee3
Thorsten Lorenz <thlorenz@gmx.de>
2012-08-20 08:14:45 -0400

    added jquery

--- static/js/github-projects.js
+++ static/js/github-projects.js
@@ -1,3 +1,4 @@
 require(['jquery'], function($) {
   console.log('github-projects', $);
+  console.log('hello');
 });
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,9 @@
 requirejs.config ({
     paths: {
-        'jquery' : 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min'
+        // 'jquery' : 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min'
+      
+        // About 200ms faster than getting it from cdn
+        'jquery' : 'lib/jquery-1.8.0'
     }
 });
 
