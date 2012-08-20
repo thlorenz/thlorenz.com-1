971f65ab22cc6faad180bbdae331669186e7aa0a
Thorsten Lorenz <thlorenz@gmx.de>
2012-08-20 07:32:17 -0400

    initial requirejs setup

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,7 @@
-console.log('requiring main.js');
+requirejs.config ({
+    paths: {
+        'jquery' : 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min'
+    }
+});
+
+require( ['github-projects']);
