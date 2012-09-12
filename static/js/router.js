97b6058472146d03766b6a84d680ddb059196a03
Thorsten Lorenz <thlorenz@gmx.de>
2012-09-11 23:11:26 -0400

    removed stackoverflow and changed contact to about

--- static/js/router.js
+++ static/js/router.js
@@ -2,18 +2,16 @@ define(
   [ 'director'
   , 'github-index' 
   , 'blog-index'
-  , 'stackoverflow-index'
-  , 'contact-index'
+  , 'about-index'
   , 'github-content'
   , 'blog-content'
   ], 
-function (director, github, blog, stackoverflow, contact, githubContent, blogContent) { 
+function (director, github, blog, about, githubContent, blogContent) { 
   var currentNav = null
     , navs = {
           github        :  github.init
         , blog          :  blog.init
-        , stackoverflow :  stackoverflow.init
-        , contact       :  contact.init
+        , about         :  about.init
       }
     ;
 
@@ -27,8 +25,7 @@ function (director, github, blog, stackoverflow, contact, githubContent, blogCon
   var routes = { 
         '/github'            :  function ()     { updateNav('github'); githubContent.init(); }
       , '/blog'              :  function ()     { updateNav('blog');   blogContent.init();   }
-      , '/stackoverflow'     :  function ()     { updateNav('stackoverflow'); }
-      , '/contact'           :  function ()     { updateNav('contact');       }
+      , '/about'             :  function ()     { updateNav('about');       }
       , '/github/repo/:name' :  function (name) { updateNav('github'); githubContent.init(name); }
       , '/blog/post/:name'   :  function (name) { updateNav('blog');   blogContent.init(name); }
       }
