1d4e342893352a1c7382231a67683fcc8397aab1
Thorsten Lorenz <thlorenz@gmx.de>
2012-09-21 08:15:58 -0400

    sourcing different main file when minified

--- static/js/main.build.js
+++ static/js/main.build.js
@@ -1,10 +1,14 @@
 requirejs.config ({
   // requirejs likes to see me repeat myself, so we have to tell it where to get these here as well
   // although we already did so in the requirejs.js-config
-  paths: {
-    'jquery'     :  '//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min'
-  , 'underscore' :  '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min'
-  }
+    shim: {  
+      'handlebars': { exports: 'Handlebars' } 
+    , 'underscore': { exports: '_' }
+    }
+  , paths: {
+      'jquery'     :  '//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min'
+    , 'underscore' :  '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min'
+    }
 });
 
 require( 
