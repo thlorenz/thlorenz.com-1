55e0a44589d5e35a328714e6a6a8fcb7d69dd61a
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-12 21:10:48 -0500

    commenting on send

--- send/index.js
+++ send/index.js
@@ -3,6 +3,17 @@ var sendJson = require('./json')
   , sendHtml = require('./html')
   ;
 
+/**
+ * Renders the page as html or sends a JSON response based on the requests's accept headers.
+ * 
+ * @name send
+ * @function
+ * @param req {Object} http request
+ * @param res {Object} http response 
+ * @param model {Object} model to be used when as context to the templates
+ * @param sidebarTmpl {String} name of the template to be used for the sidebar area of the page
+ * @param contentTmpl {String} name of the template to be used for the content area of the page
+ */
 module.exports = function send(req, res, model, sidebarTmpl, contentTmpl) {
   var prefersHtml = req.accepts('html, json') === 'html'
     , sendHtmlOrJson = prefersHtml ? sendHtml : sendJson;
