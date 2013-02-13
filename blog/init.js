9ebec96612a363e82108bcc797657a97e4c7253b
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-12 22:47:33 -0500

    copying and loading blog styles

--- blog/init.js
+++ blog/init.js
@@ -9,6 +9,28 @@ var fs         =  require('fs')
   , root       =  path.join(__dirname, '..')
   , blogRoot   =  path.join(root, 'thlorenz.com-blog')
   ;
+/**
+ * Copies srcFile to tgtFile without checking if paths are valid
+ * 
+ * @name copyFile
+ * @function
+ * @param srcFile {String}
+ * @param tgtFile {String}
+ * @param cb {Function} called when file is completely copied or an error occurs
+ */
+function copyFile(srcFile, tgtFile, cb) {
+  var readStream = fs.createReadStream(srcFile)
+    , writeStream = fs.createWriteStream(tgtFile); 
+
+  writeStream
+    .on('close', cb)
+    .on('error', cb); 
+
+  readStream
+    .on('error', cb);
+
+  readStream.pipe(writeStream);
+}
 
 function gitClone (cb) {
   log.info('blog', 'git clone', 'started');
@@ -28,12 +50,59 @@ function gitClone (cb) {
 function gitPull (cb) {
   log.info('blog', 'git pull', 'started');
 
-  exec('cd ' + blogRoot + ' && git pull origin master', function (err, res) {
-    if (err) { log.error('blog', 'git pull', err); cb(err); return; }
-    
-    log.info('blog', 'git pull', res); 
-    cb();
+  gitClone(function (err) {
+    if (err) return cb(err);
+
+    exec('cd ' + blogRoot + ' && git pull origin master', function (err, res) {
+      if (err) { log.error('blog', 'git pull', err); cb(err); return; }
+      
+      log.info('blog', 'git pull', res); 
+      cb();
+    });
   });
 }
 
-gitClone(gitPull.bind(null, function () { console.log('pulled'); }));
+function initStyles (cb) {
+  log.info('blog', 'init styles', 'started');
+  provider.concatenateStyles(function (err, css) {
+    if (err) { log.error('blog', 'init styles',  err); return cb(err); }
+
+    cb(null, css);
+  });
+}
+
+function copyStyles(styles, cb) {
+  var blogStyles = path.join(root, 'public', 'css', 'blog')
+    , tasks = styles.length
+    , abort = false;
+
+  styles.forEach(function (style) {
+    var fileName = path.basename(style);
+    if (abort) return;
+    copyFile(style, path.join(blogStyles, fileName), function (err, res) {
+      if (abort) return;
+      if (err) { abort = true; return cb(err); }
+      if (!--tasks) return cb(); 
+    });
+  });
+}
+
+function init(cb) {
+
+  gitPull(function (err) {
+    if (err) return cb(err);
+
+    provider
+      .provideFrom(blogRoot)
+      .getStylesFiles(function (err, styles) {
+        copyStyles(styles, onStylesCopied);
+      });
+  });
+
+  function onStylesCopied(err) {
+    cb(err);
+  }
+}
+
+init(function (err) { console.log('inited', err); });
+
--- config.js
+++ config.js
@@ -6,6 +6,9 @@ module.exports = {
           'bootstrap-responsive.css'
         , 'bootstrap.css'
         , 'main.css'
+        , 'blog/blog.css'
+        , 'blog/code.css'
+        , 'blog/code-fixes.css'
       ]
     , logLevel: 'verbose'
     }
@@ -14,6 +17,9 @@ module.exports = {
           'bootstrap-responsive.min.css'
         , 'bootstrap.min.css'
         , 'main.css'
+        , 'blog/blog.css'
+        , 'blog/code.css'
+        , 'blog/code-fixes.css'
       ]
     , logLevel: 'info'
     }
