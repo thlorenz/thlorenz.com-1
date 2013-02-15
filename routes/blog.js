d596ee620dbeb79a9dcc97b64bc7284b85da5679
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-15 08:02:12 -0500

    better sidebar link hover and indicating active sidebar linke

--- routes/blog.js
+++ routes/blog.js
@@ -5,10 +5,15 @@ var log  =  require('npmlog')
   , moment = require('moment')
   ;
 
-function adapt(metadata) {
+function adapt(metadata, postName) {
   return metadata.map(function (x) {
     var d = moment(x.created);
-    return { title: x.title, name: x.name, created: d.format('dddd, MMMM Do YYYY') };
+    return { 
+        title: x.title
+      , name: x.name
+      , created: d.format('dddd, MMMM Do YYYY')
+      , active: x.name === postName 
+    };
   });
 }
 
@@ -18,8 +23,13 @@ module.exports = function (app) {
       res.redirect('/blog/' + blog.getPost().name);
     })
     .get('/blog/:post', function (req, res) {
-      log.verbose('blog', 'getting post', req.params.post);
-      var model = { sidebar: adapt(blog.getMetadata()), content: blog.getPost(req.params.post) };
+      var postName = req.params.post;
+      log.verbose('blog', 'getting post', postName);
+
+      var model = { 
+          sidebar: adapt(blog.getMetadata(), postName)
+        , content: blog.getPost(postName) 
+      };
       send(req, res, model, 'blog_nav', 'blog_post');
     });
 };
