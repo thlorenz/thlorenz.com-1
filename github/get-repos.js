d50122496579a8289e194580c854814d9d6edfeb
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-18 17:44:30 -0500

    using github token when requesting repos if it is found in environment

--- github/get-repos.js
+++ github/get-repos.js
@@ -3,6 +3,7 @@
 var request = require('request')
   , log = require('npmlog')
   , moment = require('moment')
+  , url = require('url')
   , cache;
 
 
@@ -10,8 +11,32 @@ function byStarsDescending (a, b) {
   return a.stars > b.stars ? -1 : 1; 
 }
 
+function createRequest() {
+  // see: https://help.github.com/articles/creating-an-oauth-token-for-command-line-use
+  // and: https://github.com/dscape/ghcopy/blob/68981f04be58d0412e75e6bff83a7c993b6281e7/bin/ghcopy#L138-L145  
+  var req = {
+      method: 'GET'
+    , uri : url.format(
+      { protocol: 'https'
+      , hostname: 'api.github.com'
+      , pathname: '/users/thlorenz/repos?per_page=500'
+      })
+    }
+  , githubToken = process.env.GITHUB_TOKEN;
+
+  if (!githubToken) return req;
+
+  // use token to make authorized request, otherwise we'll run into exceeded API limit problem 
+  // when running on nodejitsu
+  log.silly('github', 'creating request using token');
+  req.headers = { Authorization: 'bearer ' + githubToken };
+
+  return req;
+}
+
 module.exports = function getRepos(cb) {
-  var cachedReposAreGood = cache && new Date() < cache.goodUntil;
+  var cachedReposAreGood = cache && new Date() < cache.goodUntil
+    , req = createRequest();
 
   if (cachedReposAreGood) {
     log.silly('github', 'serving from cache');
@@ -20,7 +45,8 @@ module.exports = function getRepos(cb) {
 
   log.silly('github', 'querying github to refresh cache');
 
-  request.get('https://api.github.com/users/thlorenz/repos?per_page=500', function (err, res, body) { 
+  // request.get('https://api.github.com/users/thlorenz/repos?per_page=500', function (err, res, body) { 
+  request(req, function (err, res, body) {
     if (err) return cb(err);
     var repos;
     try {
