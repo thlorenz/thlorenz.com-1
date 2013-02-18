1bf979821927b2aadde36ed88c75a6c2a64b1b55
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-18 11:04:33 -0500

    tracking individual pages

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,5 +1,6 @@
 'use strict';
 /*jshint browser:true*/
+/*global _gaq */
 
 var $ = require('jquery')
   , $sidebar
@@ -7,6 +8,11 @@ var $ = require('jquery')
   , $mainNav
   , navigatedListeners = []
   ;
+
+function trackCurrentLocation() {
+  // See: http://akahn.net/2010/11/12/tracking-events-with-google-analytics.html
+  _gaq && _gaq.push(['_trackPageview', window.location.pathname]);
+}
 
 function browserSupportsHistoryApi(history) {
   return !!(history && history.pushState);
@@ -28,6 +34,7 @@ function handleNavigation (history, url) {
     , dataType: 'json'
     })
     .success(function (res) { 
+      trackCurrentLocation();
       render(history, url, res); 
       navigatedListeners.forEach(function (fn) {
         fn(url);  
@@ -46,6 +53,7 @@ if (!browserSupportsHistoryApi(window.history)) return;
 
 window.onpopstate = function (args) { 
   if (!args.state) return;
+  trackCurrentLocation();
   update(args.state.sidebar, args.state.content);
 };
 
@@ -56,6 +64,7 @@ $mainNav = $('nav .main');
 
 $('.main .sidebar')
   .on('click', 'a', function(event) { return handleNavigation(history, this.href, true); });
+
 $mainNav
   .on('click', 'a', function(event) { 
     $mainNav.find('a').removeClass('active');
